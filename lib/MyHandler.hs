93da30182f7e74845ecadfbba9a11a1e08179fba
Dan Dart <git@dandart.co.uk>
2025-01-10 02:37:53 +0000

    so far

--- lib/MyHandler.hs
+++ lib/MyHandler.hs
@@ -1,25 +1,26 @@
-{-# LANGUAGE DeriveAnyClass  #-}
-{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE OverloadedLists     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module MyHandler where
 
 -- import Control.Error.Util
 import Control.Exception
--- import Control.Monad
+import Control.Monad
 -- import Control.Monad.Catch
--- import Control.Monad.Except
+import Control.Monad.Except
 import Control.Monad.IO.Class
--- import Crypto.Sign.Ed25519
+import Crypto.Sign.Ed25519
 import Data.Aeson
--- import Data.ByteString.Char8 qualified as BSB
--- import Data.ByteString qualified as BSW -- for ByteStringWord
--- import Data.Map                                     qualified as M
+import Data.ByteString.Char8 qualified as BSB
+import Data.ByteString qualified as BSW -- for ByteStringWord
+import Data.Map                                     qualified as M
 import GHC.Generics
 import Network.DigitalOcean.CloudFunctions.Request  as Request
 import Network.DigitalOcean.CloudFunctions.Response as Response
--- import System.Environment
--- import System.IO.Error
--- import Data.Aeson (Options(fieldLabelModifier))
+import System.Environment
+import System.IO.Error
+import Data.Aeson (Options(fieldLabelModifier))
 
 data HttpException = HttpException {
     getCode    :: Int,
@@ -30,7 +31,7 @@ data HttpException = HttpException {
 
 type DiscordWebhookPayloadType = Int
 
-discordWebhookPayloadTypePing :: DiscordWebhookPayloadType
+discordWebhookPayloadTypePing ∷ DiscordWebhookPayloadType
 discordWebhookPayloadTypePing = 1
 
 newtype DiscordWebhookRequest = DiscordWebhookRequest {
@@ -52,84 +53,48 @@ instance ToJSON DiscordWebhookResponse where
 
 
 myHandler ∷ (MonadIO m) ⇒ Request String → m (Response (Maybe DiscordWebhookResponse))
-myHandler Request { path = _path', Request.headers = _headers', method = _method', http = _http', args = _args', ctx = _ctx' } = do
+myHandler Request { path = _path', Request.headers = headers', method = method', http = _http', args = args', ctx = _ctx' } = do
     -- don't overcomplicate it
-    -- TODO short circuiting afterwards
-
-    pure $ Response {
-        body = Just (DiscordWebhookResponse discordWebhookPayloadTypePing),
-        statusCode = 200,
-        Response.headers = [
-            ("Content-Type", "application/json")
-            ]
-    }
-{-}
-
     mPubKey <- PublicKey . BSW.pack <$> lookupEnv "DISCORD_PUBLIC_KEY"
 
     case mPubKey of
-        Nothing -> pure $ Response {
-            body = Just DiscordWebhookResponse "Missing public key",
-            statusCode = 500,
-            Response.headers = [
-                ("Content-Type", "application/json")
-                ]
-        }
+        Nothing -> internalServerError "Missing public key"
         Just pubKey -> do
-
             let mReqData = decodeStrict args' :: Maybe DiscordWebhookRequest
-
             case mReqData of
-                Nothing -> pure $ Response {
-                    body = Just DiscordWebhookResponse "Invalid JSON",
-                    statusCode = 400,
-                    Response.headers = [
-                        ("Content-Type", "application/json")
-                        ]
-                }
+                Nothing -> badRequest "Invalid JSON"
                 Just reqData ->
-                    if _type reqData == discordWebhookRequestTypePing && method' == "POST"
+                    if _type reqData == discordWebhookPayloadTypePing && method' == "POST"
                     then
-                        pure $ Response {
-                            body = Nothing,
-                            statusCode = 204,
-                            Response.headers = [
-                            ]
-                        }
-                    else
-                        let
-                            msSignature = Signature . BSW.pack <$> M.lookup "X-Signature-Ed25519" headers'
-
-                            msTimestamp = BSW.pack <$> M.lookup "X-Signature-Timestamp" headers'
-                        in
-                            pure $ Response {
-                                body = Just DiscordWebhookResponse "OK",
-                                statusCode = 200,
-                                Response.headers = [
-                                        ("Content-Type", "application/json")
-                                        ]
-                            }
--}
-    {-result <- runExceptT $ do
-        signature <- ExceptT . pure . note (HttpException 400 "No signature found") $ Signature . BSW.pack <$> M.lookup "X-Signature-Ed25519" headers'
-        timestamp <- ExceptT . pure . note (HttpException 400 "No timestamp found") $ BSW.pack <$> M.lookup "X-Signature-Timestamp" headers'
-        publicKey <- ExceptT . pure . note (HttpException 500 "No public key found") $ PublicKey mPubKey
-        when . not (dverify publicKey signedMessage signature) . throwError $ HttpException 401 "Invalid signature"
-        let rawBody = encode args'
-        pure "Hello World!"
-    case result of
-        Left ex -> pure $ Response {
-            body = getMessage ex,
-            statusCode = getCode ex,
+                        noContent
+                    else do
+                        let msSignature = Signature . BSW.pack <$> M.lookup "X-Signature-Ed25519" headers'
+                        case msSignature of
+                            Nothing -> badRequest "No signature"
+                            Just signature -> do
+                                let msTimestamp = BSW.pack <$> M.lookup "X-Signature-Timestamp" headers'
+                                case msTimestamp of
+                                    Nothing -> badRequest "No timestamp"
+                                    Just timestamp -> do
+                                        let verify = dverify pubKey signedMessage signature
+                                        if verify
+                                            then ok
+                                            else forbidden "Invalid signature"
+    where
+        response status resp = pure $ Response {
+            body = Just (DiscordWebhookResponse resp),
+            statusCode = status,
             Response.headers = [
                 ("Content-Type", "application/json")
                 ]
         }
-        Right msg -> pure $ Response {
-            body = msg,
-            statusCode = 200,
+        internalServerError = response 500
+        badRequest = response 400
+        forbidden = response 401
+        ok = response 200
+        noContent = pure $ Response {
+            body = Nothing,
+            statusCode = 204,
             Response.headers = [
-                ("Content-Type", "application/json")
-                ]
+            ]
         }
-        -}
